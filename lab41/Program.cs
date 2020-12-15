01bd067ccccb0e3d1109da80fca9d9c010504fdb
Victoris <52378018+sviridchik@users.noreply.github.com>
2020-12-15 21:32:48 +0300

    Add files via upload

---
+++ lab41/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+using DataAccessLayer;
+using FileManager;
+using ServiceLayer;
+
+namespace lab41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            var logger = new Logger();
+
+
+            AccessAndConnection serviceLayer = new AccessAndConnection();
+
+            Thread loggerThread = new Thread(new ThreadStart(logger.Start));
+            Thread loggerThreaddb = new Thread(new ThreadStart(serviceLayer.StartAccess));
+
+            loggerThread.Start();
+            loggerThreaddb.Start();
+
+
+        }
+    }
+}
